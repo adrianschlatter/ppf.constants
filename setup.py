40c6175aff4627dd391e5df1e6cbe229e049ac38
Adrian Schlatter <schlatter@phys.ethz.ch>
2014-12-14 14:26:22 +0100

    Initial release of pyconstants.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+pack_name='pyconstants'
+pack_version='0.3'
+pack_description='Important pysical constants'
+pack_author='Simon Zeller, Adrian Schlatter'
+
+setup(name=pack_name,version=pack_version,description=pack_description,
+      author=pack_author,py_modules=["constants"])
+
