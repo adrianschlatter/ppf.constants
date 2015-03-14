b94e7f4eeed3287cc596a733b62335e831ce316e
Adrian Schlatter <schlatter@phys.ethz.ch>
2015-03-14 17:40:35 +0100

    changed permissions of constants.py because it is installed with these permissions (and should therefore be readable by everyone).

--- setup.py
+++ setup.py
@@ -2,9 +2,12 @@ from distutils.core import setup
 
 pack_name='pyconstants'
 pack_version='0.3'
-pack_description='Important pysical constants'
+pack_description='Important physical constants'
 pack_author='Simon Zeller, Adrian Schlatter'
 
-setup(name=pack_name,version=pack_version,description=pack_description,
-      author=pack_author,py_modules=["constants"])
+setup(	name=pack_name,
+	version=pack_version,
+	description=pack_description,
+	author=pack_author,
+	py_modules=[ 'constants' ], )
 
